47222f4bf92a499acfbb5022e9926cf3b8e5b1b3
ErikkunGA <141973843+ErikkunGA@users.noreply.github.com>
2025-05-30 14:02:06 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,51 @@
+function sortearNumero(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let numuru=(sortearNumero(1, 10));
+
+
+let progresso = 1;
+function atualizarProgresso() {
+    if (progresso < 100) {
+        progresso += 1;
+        document.getElementById("progress-bar").style.width = progresso + "%";
+        document.getElementById("progress-text").textContent = progresso
+    }
+}
+function destualizarProgresso(){
+    if (progresso > 1){
+        progresso = 1;
+        document.getElementById("progress-bar").style.width = progresso + "%";
+        document.getElementById("progress-text").textContent = progresso
+    }
+}
+let albafeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[{]};:'\",<.>/?\\|`~"
+function silvao() {
+    let senha = ""
+    for (let i = 0; i < progresso; i++){
+        let sorte = sortearNumero(1, 98)
+        let nhase = albafeto[sorte];
+        senha += nhase
+    }
+    document.getElementById("resultado").textContent = "A senha é: " + senha;
+}
+const imagens = [
+    "./img/imagem (1).png",
+    "./img/imagem (2).png",
+    "./img/imagem (3).png",
+    "./img/imagem (4).png",
+    "./img/imagem (5).png",
+    "./img/imagem (6).png",
+    "./img/imagem (7).png",
+    "./img/imagem (8).png",
+    "./img/imagem (9).png",
+    "./img/imagem (10).png",
+    "./img/imagem (11).png"
+]
+function sortearImagem() {
+    const indice = sortearImagem(1, 11);
+    document.getElementById("background").style.backgroundImage = `url(/img${imagem[indice]})`;
+    alert(indice);
+}
+sortearImagem();
+
